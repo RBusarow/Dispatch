9752bd0a5660995dfdc946315a13edcdfa5c323f
Rick Busarow <RBusarow@users.noreply.github.com>
2020-01-01 23:59:45 -0600

    kotlin gradle dsl (#9)

---
+++ sample/build.gradle.kts
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2019-2020 Rick Busarow
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+  id("com.android.application")
+  id("kotlin-android")
+  id("kotlin-android-extensions")
+}
+
+buildscript {
+
+  repositories {
+    mavenCentral()
+    google()
+    jcenter()
+    maven(url = "https://jitpack.io")
+  }
+
+}
+
+android {
+  compileSdkVersion(Versions.compileSdk)
+
+  defaultConfig {
+    minSdkVersion(Versions.minSdk)
+    targetSdkVersion(Versions.targetSdk)
+    versionCode = 1
+    versionName = Versions.versionName
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+}
+
+dependencies {
+
+  implementation(Libs.Androidx.activity)
+  implementation(Libs.Androidx.lifecycle)
+  implementation(Libs.Androidx.lifecycleExtensions)
+
+  implementation(Libs.JakeWharton.timber)
+
+  implementation(Libs.Kotlinx.Coroutines.core)
+  implementation(Libs.Kotlinx.Coroutines.android)
+
+  testImplementation(Libs.Kotlinx.Coroutines.test)
+
+  implementation(project(":dispatcher-provider"))
+  testImplementation(project(":dispatcher-provider-test"))
+
+  implementation(Libs.Kotlin.stdlib)
+  implementation(Libs.Androidx.appcompat)
+  implementation(Libs.Androidx.coreKtx)
+  implementation(Libs.Androidx.constraintLayout)
+
+  testImplementation(Libs.JUnit.jUnit5)
+  testImplementation(Libs.kluent)
+  androidTestImplementation(Libs.Androidx.testRunner)
+  androidTestImplementation(Libs.Androidx.testEspressoCore)
+}
