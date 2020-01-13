49cca91de94cd691f05fe939d77b578c94c02e30
Rick Busarow <rick.busarow@milwaukeetool.com>
2020-01-12 18:33:20 -0600

    update version to 1.0.0-beta2

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -24,7 +24,7 @@ object Versions {
   const val dagger = "2.25.2"
   const val kotlin = "1.3.61"
 
-  const val versionName = "1.0.0-beta01"
+  const val versionName = "1.0.0-beta02"
 }
 
 object BuildPlugins {
@@ -108,9 +108,11 @@ object Libs {
   object RickBusarow {
 
     object DispatcherProvider {
-      private const val version = "1.0.0-beta01"
-      const val core = "com.rickbusarow.dispatcherprovider:dispatcher-provider:$version"
-      const val test = "com.rickbusarow.dispatcherprovider:dispatcher-provider-test:$version"
+
+      const val core =
+        "com.rickbusarow.dispatcherprovider:dispatcher-provider:${Versions.versionName}"
+      const val test =
+        "com.rickbusarow.dispatcherprovider:dispatcher-provider-test:${Versions.versionName}"
     }
   }
 
