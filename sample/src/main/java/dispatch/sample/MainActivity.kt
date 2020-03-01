86ce4e9323faf0b37a36a58cd041e7c1fe0e29a1
Rick Busarow <rickbusarow@gmail.com>
2020-03-01 17:20:49 -0600

    add ViewBinding support and MainFragment in Sample

--- sample/src/main/java/dispatch/sample/MainActivity.kt
+++ sample/src/main/java/dispatch/sample/MainActivity.kt
@@ -19,61 +19,20 @@ package dispatch.sample
 
 import android.annotation.*
 import android.os.*
-import androidx.activity.*
 import androidx.appcompat.app.*
-import androidx.lifecycle.*
-import dispatch.core.*
-import kotlinx.android.synthetic.main.activity_main.*
+import dispatch.sample.databinding.*
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.*
 import timber.log.*
 
 @ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity() {
 
-  val scope = MainCoroutineScope()
-
-  private val factory = viewModelFactory {
-    MainViewModel(DefaultCoroutineScope(), SomeRepository(IOCoroutineScope()))
-  }
-
-  val viewModel: MainViewModel by viewModels { factory }
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(ActivityMainBinding.inflate(layoutInflater).root)
 
     Timber.plant(Timber.DebugTree())
 
-    // share the same Job as the `scope` property above, but use the IO dispatcher as default
-    val ioDefaultScope = scope + Dispatchers.IO
-
-    viewModel.message
-      .onEach { Timber.v("I'm using the default dispatcher!") }
-      // extract the default dispatcher from the CoroutineScope and apply it upstream
-      .flowOn(scope.defaultDispatcher)
-      .onEach { message ->
-        Timber.v("I'm using the main dispatcher!")
-        tvMessage.text = message
-      }
-      .onCompletion { tvMessage.text = "All done!" }
-      // the .flowOn____() operator pulls the desired dispatcher out of the CoroutineScope
-      // and applies it.  So in this case .flowOnMain() is pulling
-      // the dispatcher assigned to "main" out of `ioDefaultScope`
-      // and dispatching upstream execution
-      .flowOnMain()
-      .onEach { Timber.v("I'm using the IO dispatcher!") }
-      // the default dispatcher in this scope is now Dispatchers.IO
-      .launchIn(ioDefaultScope)
-
   }
 }
-
-@Suppress("UNCHECKED_CAST")
-inline fun <reified VM : ViewModel> viewModelFactory(crossinline f: () -> VM):
-    ViewModelProvider.Factory =
-  object : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(aClass: Class<T>): T = f() as T
-  }
-
 
