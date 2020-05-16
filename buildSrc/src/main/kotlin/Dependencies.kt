c9108e3ddf0db5e66feb90c94aee63845ce6f6e2
Rick Busarow <rickbusarow@gmail.com>
2020-05-16 01:32:31 -0500

    coroutines 1.3.3 -> 1.3.6

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -164,7 +164,7 @@ object Libs {
   object Kotlinx {
 
     object Coroutines {
-      private const val version = "1.3.3"
+      private const val version = "1.3.6"
       const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
       const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
       const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
