e17dc7f2890d3b35d6dd702221204ec87d8fa9d0
Rick Busarow <rickbusarow@gmail.com>
2021-05-16 22:17:26 -0500

    update Detekt to 1.17.0 https://github.com/detekt/detekt/releases/tag/v1.17.0

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -33,17 +33,6 @@ dependencyResolutionManagement {
   repositories {
     google()
     mavenCentral()
-    jcenter {
-      content {
-        // dokka
-        includeGroup("org.jetbrains.dokka")
-        includeModule("org.jetbrains", "markdown")
-        // Detekt && dokka
-        includeModule("org.jetbrains.kotlinx", "kotlinx-html-jvm")
-        // https://youtrack.jetbrains.com/issue/IDEA-261387
-        includeModule("org.jetbrains.trove4j", "trove4j")
-      }
-    }
   }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,10 +38,7 @@ dependencyResolutionManagement {
         // dokka
         includeGroup("org.jetbrains.dokka")
         includeModule("org.jetbrains", "markdown")
-        // Detekt && dokka
         includeModule("org.jetbrains.kotlinx", "kotlinx-html-jvm")
-        // https://youtrack.jetbrains.com/issue/IDEA-261387
-        includeModule("org.jetbrains.trove4j", "trove4j")
       }
     }
   }
